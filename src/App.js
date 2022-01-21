a743a35c23f17924f793d1c0d1e488827f55318c
Haren Sah <haren3035@gmail.com>
2022-01-21 11:23:11 +1100

    breaking the chunk data into smaller pagination data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  const [page, setPage ] = useState(0)
+  const [followers, setFollowers ] = useState([])
+
+  useEffect(() => {
+    if(loading) return 
+    setFollowers(data[page])
+  }, [loading])
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='inderline'></div>
+        </div>
+        <section className='followers'>
+          <div className='container'>
+          { followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />
+            })}
+          </div>
+          </section>
+    </main>
+  )
 }
 
 export default App
